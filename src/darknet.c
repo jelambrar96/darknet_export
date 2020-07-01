f6c34edbcdb3260eafac53c3ac3bb139b3e1216f
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-07-01 11:37:14 +0200

    Update from Alexey master
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- src/darknet.c
+++ src/darknet.c
@@ -571,6 +571,11 @@ int main(int argc, char **argv)
 {
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    printf(" _DEBUG is used \n");
+#endif
+
+#ifdef DEBUG
+    printf(" DEBUG=1 \n");
 #endif
 
 	int i;
